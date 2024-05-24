13e1bea5426daf6f24eb191d81586ba1dec5e1c0
VerVelVel <vera.velikoborets@gmail.com>
2024-05-24 12:40:40 +0300

    no sadness

--- pages/4_🌲_Forest_Aerial_Segmentation.py
+++ pages/4_🌲_Forest_Aerial_Segmentation.py
@@ -4,6 +4,12 @@ import numpy as np
 from PIL import Image
 import requests
 from io import BytesIO
+from pathlib import Path
+import sys
+import cv2
+project_root = Path(__file__).resolve().parents[1]
+sys.path.append(str(project_root))
+
 from models.model3.model_unet import load_custom_unet_model
 
 model_path = 'my_unet_model.h5'
@@ -15,8 +21,11 @@ def load_and_prepare_image(image, target_size=(128, 128)):
         image = image.convert("RGB")
     image = image.resize(target_size, Image.Resampling.LANCZOS)
     image = img_to_array(image)
+    image = image / 255.0  #new
     image = np.expand_dims(image, axis=0)
     return image
+
+
 
 def predict(image):
     """ Прогнозирует маску сегментации из изображения. """
@@ -24,11 +33,20 @@ def predict(image):
     prediction = model.predict(processed_image)
     return prediction[0]
 
-def display_prediction(prediction):
-    prediction = prediction.squeeze()  # Удаление лишних размерностей
-    mask = (prediction > 0.5).astype(np.uint8)  # Применение порога для создания бинарной маски
-    prediction_image = (mask * 255).astype(np.uint8)  # Масштабирование для отображения
-    return Image.fromarray(prediction_image)
+# def display_prediction(prediction):
+#     prediction = prediction.squeeze()  # Удаление лишних размерностей
+#     mask = (prediction > 0.5).astype(np.uint8)  # Применение порога для создания бинарной маски
+#     prediction_image = (mask * 255).astype(np.uint8)  # Масштабирование для отображения
+#     return Image.fromarray(prediction_image)
+
+def display_prediction(image, prediction):
+    prediction = prediction.squeeze()
+    mask = (prediction > 0.5).astype(np.uint8)
+    mask = cv2.resize(mask, (image.width, image.height))  # Resize mask to the original image size
+    color_mask = np.zeros_like(image)
+    color_mask[mask == 1] = [255, 0, 0]  # Red color for mask
+    combined_image = cv2.addWeighted(np.array(image), 1, color_mask, 0.5, 0)  # Overlay mask on image
+    return Image.fromarray(combined_image)
 
 
 def load_image_from_url(url):
@@ -47,7 +65,7 @@ if uploaded_file is not None:
     # Обработка изображения и предсказание
     processed_image = load_and_prepare_image(image)
     prediction = model.predict(processed_image)
-    display_image = display_prediction(prediction[0])
+    display_image = display_prediction(image, prediction[0])
     st.image(display_image, caption='Segmented Image', use_column_width=True)
 
 url = st.text_input("Enter the URL of an image...")
