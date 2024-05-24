5d4a2537710ff1080c0875494d72d140396cb9a8
darina afanaseva <dafanasiva@gmail.com>
2024-05-24 12:16:43 +0300

    sadness

--- models/model3/model_unet.py
+++ models/model3/model_unet.py
@@ -1,57 +1,31 @@
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose
-from tensorflow.keras.optimizers import Adam
+# Импорт необходимых библиотек
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Conv2DTranspose
 
-def unet(input_size=(256, 256, 1)):
-    inputs = Input(input_size)
-    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    
-    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    
-    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    
-    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-    
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-    
-    up6 = Conv2DTranspose(512, 2, strides=(2, 2), padding='same')(conv5)
-    merge6 = concatenate([up6, conv4], axis=3)
-    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
-    
-    up7 = Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(conv6)
-    merge7 = concatenate([up7, conv3], axis=3)
-    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
-    
-    up8 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(conv7)
-    merge8 = concatenate([up8, conv2], axis=3)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
-    
-    up9 = Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(conv8)
-    merge9 = concatenate([up9, conv1], axis=3)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    
-    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
 
-    model = Model(inputs=inputs, outputs=conv10)
-    # model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+# Функция для загрузки модели с кастомизированными настройками
+def load_custom_unet_model(model_path):
+    """
+    Загружает модель U-Net, исправляя конфликты с неизвестными аргументами в слоях.
     
+    Args:
+    model_path (str): Путь к файлу модели .h5
+    
+    Returns:
+    tf.keras.Model: Загруженная модель U-Net.
+    """
+    # Определение custom_objects для исправления ошибок при загрузке
+    custom_objects = {
+        'Conv2DTranspose': lambda **kwargs: Conv2DTranspose(**{key: arg for key, arg in kwargs.items() if key != 'groups'})
+    }
+    
+    # Загрузка модели с custom_objects
+    model = load_model(model_path, custom_objects=custom_objects)
     return model
 
-def load_pretrained_model(weights_path):
-    model = unet()
-    model.load_weights(weights_path)
-    return model
+# Тестирование кода, если файл запускается как основной скрипт
+if __name__ == "__main__":
+    model_path = 'my_unet_model.h5'  # Укажите правильный путь к файлу модели
+    model = load_custom_unet_model(model_path)
+    print("Модель успешно загружена.")
+    model.summary()  # Вывод информации о модели
--- pages/4_🌲_Forest_Aerial_Segmentation.py
+++ pages/4_🌲_Forest_Aerial_Segmentation.py
@@ -1,58 +1,62 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.utils import img_to_array, load_img
 import numpy as np
-import requests
 from PIL import Image
+import requests
 from io import BytesIO
+from models.model3.model_unet import load_custom_unet_model
 
-from models.model3.model_unet import load_pretrained_model
+model_path = 'my_unet_model.h5'
+model = load_custom_unet_model(model_path)
 
-model_path = 'my_unet_model.h5'  
-model = load_pretrained_model(model_path)
-# Загрузка модели
-# model = load_model('my_unet_model.h5')
-
-def load_and_prepare_image(image, target_size=(256, 256)):
+def load_and_prepare_image(image, target_size=(128, 128)):
+    """ Загружает и подготавливает изображение для модели. """
     if image.mode != 'RGB':
         image = image.convert("RGB")
-    image = image.resize(target_size)
+    image = image.resize(target_size, Image.Resampling.LANCZOS)
     image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)  # Модель ожидает размерность (1, 256, 256, 3)
+    image = np.expand_dims(image, axis=0)
     return image
 
 def predict(image):
-    image = load_and_prepare_image(image)
-    prediction = model.predict(image)
+    """ Прогнозирует маску сегментации из изображения. """
+    processed_image = load_and_prepare_image(image)
+    prediction = model.predict(processed_image)
     return prediction[0]
 
+def display_prediction(prediction):
+    prediction = prediction.squeeze()  # Удаление лишних размерностей
+    mask = (prediction > 0.5).astype(np.uint8)  # Применение порога для создания бинарной маски
+    prediction_image = (mask * 255).astype(np.uint8)  # Масштабирование для отображения
+    return Image.fromarray(prediction_image)
+
+
 def load_image_from_url(url):
+    """ Загружает изображение по URL. """
     response = requests.get(url)
     image = Image.open(BytesIO(response.content))
     return image
 
 st.title('U-Net Model Deployment for Image Processing')
 
-# Загрузка файла с компьютера
 uploaded_file = st.file_uploader("Upload an image...", type=['jpg', 'png'])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
-    prediction = predict(image)
-    st.image(prediction, caption='Processed Image', use_column_width=True)
+    
+    # Обработка изображения и предсказание
+    processed_image = load_and_prepare_image(image)
+    prediction = model.predict(processed_image)
+    display_image = display_prediction(prediction[0])
+    st.image(display_image, caption='Segmented Image', use_column_width=True)
 
-# Загрузка изображения по URL
 url = st.text_input("Enter the URL of an image...")
 if url:
     try:
         image = load_image_from_url(url)
         st.image(image, caption='Image from URL', use_column_width=True)
         prediction = predict(image)
-        st.image(prediction, caption='Processed Image', use_column_width=True)
+        display_image = display_prediction(prediction)
+        st.image(display_image, caption='Processed Image', use_column_width=True)
     except Exception as e:
         st.error(f"Error loading image from URL: {e}")
-
-# Запуск Streamlit
-if __name__ == '__main__':
-    st.run()
-
